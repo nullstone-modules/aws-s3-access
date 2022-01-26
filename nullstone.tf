5fb89ed84cc86d917d92142019014152ab2e4f33
Scott Sickles <scottsickles@gmail.com>
2022-01-26 13:27:10 -0500

    updated connection

--- nullstone.tf
+++ nullstone.tf
@@ -8,13 +8,13 @@ terraform {
 
 data "ns_workspace" "this" {}
 
-data "ns_connection" "s3" {
-  name = "s3"
-  type = "s3/aws"
+data "ns_connection" "s3_bucket" {
+  name = "s3_bucket"
+  type = "aws-s3"
 }
 
 locals {
-  s3_region      = data.ns_connection.s3.outputs.region
-  s3_bucket_arn  = data.ns_connection.s3.outputs.bucket_arn
-  s3_bucket_name = data.ns_connection.s3.outputs.bucket_name
+  s3_region      = data.ns_connection.s3_bucket.outputs.region
+  s3_bucket_arn  = data.ns_connection.s3_bucket.outputs.bucket_arn
+  s3_bucket_name = data.ns_connection.s3_bucket.outputs.bucket_name
 }
