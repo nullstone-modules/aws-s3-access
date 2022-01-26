e662b9f8d7222d2149e809825db6a50d88313886
Scott Sickles <scottsickles@gmail.com>
2022-01-26 16:55:06 -0500

    squash merge C678-s3-access

---
+++ policy.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_policy" "this" {
+  name = local.resource_name
+  policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "this" {
+  role = var.app_metadata["role_name"]
+  policy_arn = aws_iam_policy.this.arn
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid     = "AllowFullAccess"
+    effect  = "Allow"
+    actions = ["s3:*"]
+
+    resources = [local.s3_bucket_arn, "${local.s3_bucket_arn}/*"]
+  }
+}
