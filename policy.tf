bdfed48022fa967caa5dec44bea9d7cc965f8ac7
Scott Sickles <scottsickles@gmail.com>
2022-01-26 14:35:25 -0500

    added the policy and attached to role

---
+++ policy.tf
@@ -0,0 +1,17 @@
+resource "aws_iam_role_policy" "access_" {
+  role   = var.app_metadata.task_role_name
+  policy = data.aws_iam_policy_document.access.json
+}
+
+data "aws_iam_policy_document" "access" {
+  statement {
+    sid     = "AllowFullAccess"
+    effect  = "Allow"
+    actions = ["s3:*"]
+
+    principals {
+      identifiers = [local.s3_bucket_arn, "${local.s3_bucket_arn}/*"]
+      type        = "AWS"
+    }
+  }
+}
